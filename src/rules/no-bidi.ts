24ff604d8e95399aca11ee6faa49fdf88d04922a
Liran Tal <liran.tal@gmail.com>
2021-11-06 23:36:52 +0200

    fix: dont die on undefined. signed, a js dev

--- src/rules/no-bidi.ts
+++ src/rules/no-bidi.ts
@@ -4,37 +4,41 @@ export function noBidi(context) {
   return {
     onCodePathStart: function (codePath, node) {
       // at the start of analyzing a code path
-      node.tokens.forEach((tokenObject) => {
-        if (
-          tokenObject.value &&
-          hasTrojanSource({ sourceText: tokenObject.value })
-        ) {
-          context.report({
-            node: node,
-            data: {
-              text: tokenObject.value.toString("utf-8"),
-            },
-            message:
-              "Detected potential trojan source attack with unicode bidi introduced in this code: '{{text}}'.",
-          });
-        }
-      });
+      if (node.tokens && Array.isArray(node.tokens)) {
+        node.tokens.forEach((tokenObject) => {
+          if (
+            tokenObject.value &&
+            hasTrojanSource({ sourceText: tokenObject.value })
+          ) {
+            context.report({
+              node: node,
+              data: {
+                text: tokenObject.value.toString("utf-8"),
+              },
+              message:
+                "Detected potential trojan source attack with unicode bidi introduced in this code: '{{text}}'.",
+            });
+          }
+        });
+      }
 
-      node.comments.forEach((tokenObject) => {
-        if (
-          tokenObject.value &&
-          hasTrojanSource({ sourceText: tokenObject.value })
-        ) {
-          context.report({
-            node: node,
-            data: {
-              text: tokenObject.value.toString("utf-8"),
-            },
-            message:
-              "Detected potential trojan source attack with unicode bidi introduced in this comment: '{{text}}'.",
-          });
-        }
-      });
+      if (node.comments && Array.isArray(node.comments)) {
+        node.comments.forEach((tokenObject) => {
+          if (
+            tokenObject.value &&
+            hasTrojanSource({ sourceText: tokenObject.value })
+          ) {
+            context.report({
+              node: node,
+              data: {
+                text: tokenObject.value.toString("utf-8"),
+              },
+              message:
+                "Detected potential trojan source attack with unicode bidi introduced in this comment: '{{text}}'.",
+            });
+          }
+        });
+      }
     },
   };
 }
