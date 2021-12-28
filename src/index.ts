ccfca07c8cb48652f2444e1cd23b76692d19838c
Matt Travi <programmer@travi.org>
2021-12-28 14:33:44 -0600

    feat: defined a `recommended` config
    fixes #2

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,12 @@ module.exports = {
       create: noBidi,
     },
   },
+  configs: {
+    recommended: {
+      plugins: ['anti-trojan-source'],
+      rules: {
+        'anti-trojan-source/no-bidi': 'error',
+      },
+    },
+  },
 };
